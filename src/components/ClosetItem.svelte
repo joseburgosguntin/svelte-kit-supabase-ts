fb6286c737fc14c529c9bcfd6161a3fd88d241b1
joseburgosguntin <josem.123burgos@gmail.com>
2022-07-20 17:48:55 -0400

    minor tweak now the closet should be dark

--- src/components/ClosetItem.svelte
+++ src/components/ClosetItem.svelte
@@ -16,9 +16,9 @@
     closetItem.completed ? 'line-through' : ''
 } -->
 
-<li class="bg-gray-700 text-white font-bold flex items-center shadow-sm border border-gray-200 rounded-lg my-2 py-2 px-4">
+<li class="bg-gray-700 text-white font-bold flex items-center shadow-sm border border-gray-600 rounded-lg my-2 py-2 px-4">
 	<Svg type={closetItem.type} fill={closetItem.color_hex} />
-	<span class={`flex-1 text-gray-800`}>
+	<span class={`flex-1`}>
 		{closetItem.color_name}{" "}{closetItem.type}
 	</span>
 	<button
